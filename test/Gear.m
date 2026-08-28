737c1a6529e89cea7fae67389c0c40757f224c9e
mTerentev <termisha@mail.ru>
2026-08-28 18:58:15 +0300

    Points evenly-sparser

--- test/Gear.m
+++ test/Gear.m
@@ -37,16 +37,43 @@ F = batchCross(dY_du, dY_dv);
 axis equal;
 hold on;
 
+%Solve Envelope
 points = zeros(2,1,v_res-1);
 for i = 1:v_res-1
   [sol, ind] = min(abs(F(1,1,i,:)));
   points(:,:,i) = Y(:,:,i,ind);
 endfor
 
+%Sparse points evenly
+TotalL = 0;
+for i = 1: v_res-2
+  TotalL += vecnorm(points(:,1,i+1)-points(:,1,i));
+endfor
 
-filtered_points = zeros(2,1,v_res-1);
+points1 = zeros(2,1,v_res);
 l=1;
-k=1;
+st_point = points(:,:,1);
+for i = 1:1000
+  path=TotalL/v_res;
+  L = vecnorm(points(:,1,l+1)-st_point);
+  while path > L
+    l++;
+    path -= L;
+    st_point = points(:,:,l);
+    L = vecnorm(points(:,1,l+1)-st_point);
+  endwhile
+  st_point = (points(:,1,l+1)-st_point)/L*path + st_point;
+  points1(:,:,i) = st_point;
+  % if vecnorm(st_point-points(:,:,v_res-1)) < 0.01 break endif
+endfor
+
+% plot(points(1,:,:), points(2,:,:));
+% plot(points1(1,:,:), points1(2,:,:), "linestyle", "-", "marker", "o");
+% waitfor(gcf);
+% drawnow;
+
+points = points1;
+
 function res = crs(x,y)
   res = x(1)*y(2)-y(1)*x(2);
 endfunction
@@ -61,12 +88,13 @@ function res = Intersection(a1,b1,a2,b2)
   res = a1*t(1)+b1*(1-t(1));
 endfunction
 
-% Intersection([1;1],[0;0],[0;1],[1;0])
-
-% intersecting_points = zeros(2,1,v_res-1);
+%Filter self-intersections out
+filtered_points = zeros(2,1,v_res-1);
+l=1;
+k=1;
 while true
-  for j = k+1 : k+100-2
-    if j > v_res-2 break; endif  
+  for j = k+10 : k+200
+    if j > v_res-2 || j <= 0 break; endif  
     if isIntersecting(points(:,1,k),points(:,1,k+1),points(:,1,j),points(:,1,j+1))
       filtered_points(:,:,l) = points(:,:,k);
       l++;
@@ -79,21 +107,9 @@ while true
   filtered_points(:,:,l) = points(:,:,k);
   l++;
   k++;
-  if k > v_res-1 break; endif  
+  if k > v_res-2 break; endif  
 endwhile
-% plot(intersecting_points(1,:,:), intersecting_points(2,:,:), "o");
 
-% while true
-
-%   [sol, ind] = find(vecnorm(points-points(:,:,k)) < 0.01, 1, "last");
-%   if ind - k > 5
-%     k += ind-k;
-%   endif
-%   filtered_points(:,:,l) = points(:,:,k);
-%   l++;
-%   k++;
-%   if k > v_res-1 break; endif
-% endwhile
 filtered_points = resize(filtered_points, 2, 1, l-1);
 plot(points(1,:,:), points(2,:,:), "linestyle", "-", "marker", "o");
 plot(filtered_points(1,:,:), filtered_points(2,:,:), "color", "red", "linewidth", 3);
